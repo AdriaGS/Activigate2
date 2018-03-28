96f7663b669db86aee378cac1852c1ef48b8a436
adria <adria@IPAL-MAC.local>
2018-03-28 17:31:41 +0800

    Fitbit Activity completed

--- app/src/main/java/fr/cnrs/ipal/activigate2/View/FitbitActivity.java
+++ app/src/main/java/fr/cnrs/ipal/activigate2/View/FitbitActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,6 +22,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import fr.cnrs.ipal.activigate2.Fitbit.OAuthToken;
 import fr.cnrs.ipal.activigate2.R;
@@ -39,11 +46,83 @@ public class FitbitActivity extends AppCompatActivity {
     String sleepData = "";
 
     ProgressDialog pd;
+    CircleFillView circleFill;
+
+    TextView lightlyActive;
+    TextView sedentaryMin;
+    TextView veryActive;
+    TextView steps;
+    TextView restingHeartRate;
+    TextView sleepDuration;
+    TextView sleepEfficiency;
+    TextView awakeningsCount;
+
+    TextView outOfRangeZone;
+    TextView fatBurnZone;
+    TextView cardioZone;
+    TextView peakZone;
+
+    TextView day;
+
+    int restingHeartRate_val;
+    int sedentaryMin_val;
+    int lightlyActiveMin_val;
+    int veryActiveMin_val;
+    int steps_val;
+    int stepsGoal;
+    int sleepDuration_val;
+    int sleepEfficiency_val;
+    int awakeningsCount_val;
+    ArrayList<Integer> minutesZones = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitbit);
+
+        lightlyActive = findViewById(R.id.lightlyActiveMinTV);
+        sedentaryMin = findViewById(R.id.sedentaryMinTV);
+        veryActive = findViewById(R.id.veryActiveMinTV);
+        steps = findViewById(R.id.steps);
+        restingHeartRate = findViewById(R.id.restingHRTV);
+        sleepDuration = findViewById(R.id.sleepDurationTV);
+        sleepEfficiency = findViewById(R.id.efficiencyTV);
+        awakeningsCount = findViewById(R.id.awakeningCountTV);
+
+        outOfRangeZone = findViewById(R.id.outOfRangeTV);
+        fatBurnZone = findViewById(R.id.fatBurnTV);
+        cardioZone = findViewById(R.id.cardioTV);
+        peakZone = findViewById(R.id.peakTV);
+
+        day = findViewById(R.id.dayTV);
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+
+        day.setText("TODAY - " + dateFormat.format(date));
+
+        circleFill = (CircleFillView) findViewById(R.id.stepsProgress);
+
+        if (getParameters()) {
+            pd = new ProgressDialog(this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+
+            getHeartRate();
+            getActivity();
+            getSleep();
+        }
+        else {
+            Log.e("Error", "Some parameters returned null");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
     private void getHeartRate() {
@@ -100,21 +179,36 @@ public class FitbitActivity extends AppCompatActivity {
 
     }
 
-    public void updateDataView() {
+    public void processResult() {
 
         if (!heartRateData.equals("") && !activityData.equals("") && !sleepData.equals("") && pd.isShowing()) {
             pd.dismiss();
 
             try {
-                JSONObject activityJson = new JSONObject(activityData);
+                JSONObject activityJson = new JSONObject(activityData).getJSONObject("summary");
                 JSONArray heartZones = activityJson.getJSONArray("heartRateZones");
-                int restingHeartRate = activityJson.getInt("restingHeartRate");
-                int sedentaryMin = activityJson.getInt("sedentaryMinutes");
-                int lightlyActiveMin = activityJson.getInt("lightlyActiveMinutes");
-                int veryActiveMin = activityJson.getInt("veryActiveMinutes");
-                int steps = activityJson.getInt("steps");
-                int stepsGoal = activityJson.getJSONObject("goals").getInt("steps");
 
+                for(int i = 0; i < heartZones.length(); i++) {
+                    String zone = heartZones.getString(i);
+                    minutesZones.add(new JSONObject(zone).getInt("minutes"));
+                }
+
+                restingHeartRate_val = activityJson.getInt("restingHeartRate");
+                sedentaryMin_val = activityJson.getInt("sedentaryMinutes");
+                lightlyActiveMin_val = activityJson.getInt("lightlyActiveMinutes");
+                veryActiveMin_val = activityJson.getInt("veryActiveMinutes");
+                steps_val = activityJson.getInt("steps");
+                stepsGoal = new JSONObject(activityData).getJSONObject("goals").getInt("steps");
+
+
+                JSONObject sleepJson = new JSONObject(sleepData);
+                String sleep = sleepJson.getJSONArray("sleep").getString(0);
+                sleepJson = new JSONObject(sleep);
+                sleepDuration_val = sleepJson.getInt("duration");
+                sleepEfficiency_val = sleepJson.getInt("efficiency");
+                awakeningsCount_val = sleepJson.getInt("awakeningsCount");
+
+                updateView();
 
 
             } catch (JSONException e) {
@@ -122,6 +216,44 @@ public class FitbitActivity extends AppCompatActivity {
             }
 
         }
+
+    }
+
+    private void updateView() {
+
+        int steps_percent = steps_val*100/stepsGoal;
+
+        steps.setText("Steps\n" + String.valueOf(steps_val));
+        circleFill.setValue(steps_percent);
+
+        lightlyActive.setText(String.valueOf(String.format("%02d:%02d H",
+                TimeUnit.MINUTES.toHours(lightlyActiveMin_val),
+                lightlyActiveMin_val - TimeUnit.HOURS.toMinutes(TimeUnit.MINUTES.toHours(lightlyActiveMin_val)))));
+        sedentaryMin.setText(String.valueOf(String.format("%02d:%02d H",
+                TimeUnit.MINUTES.toHours(sedentaryMin_val),
+                sedentaryMin_val - TimeUnit.HOURS.toMinutes(TimeUnit.MINUTES.toHours(sedentaryMin_val)))));
+        veryActive.setText(String.valueOf(String.format("%02d:%02d H",
+                TimeUnit.MINUTES.toHours(veryActiveMin_val),
+                veryActiveMin_val - TimeUnit.HOURS.toMinutes(TimeUnit.MINUTES.toHours(veryActiveMin_val)))));
+
+        int totalMinutes = 0;
+        for(int i = 0; i < minutesZones.size(); i++) {
+            totalMinutes += minutesZones.get(i);
+        }
+
+        restingHeartRate.setText(String.valueOf(restingHeartRate_val) + " bpm");
+        outOfRangeZone.setText(String.valueOf(minutesZones.get(0)*100/totalMinutes) + "%");
+        fatBurnZone.setText(String.valueOf(minutesZones.get(1)*100/totalMinutes) + "%");
+        cardioZone.setText(String.valueOf(minutesZones.get(2)*100/totalMinutes) + "%");
+        peakZone.setText(String.valueOf(minutesZones.get(3)*100/totalMinutes) + "%");
+
+        sleepDuration.setText(String.format("%02d hours, %02d min",
+                TimeUnit.MILLISECONDS.toHours(sleepDuration_val),
+                TimeUnit.MILLISECONDS.toMinutes(sleepDuration_val) -
+                        TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(sleepDuration_val))
+        ));
+        sleepEfficiency.setText(String.valueOf(sleepEfficiency_val) + "%");
+        awakeningsCount.setText(String.valueOf(awakeningsCount_val));
 
     }
 
@@ -194,7 +326,7 @@ public class FitbitActivity extends AppCompatActivity {
             if(result == null) {
                 Log.e("Error", "Error on data reception");
             }
-            updateDataView();
+            processResult();
         }
 
     }
