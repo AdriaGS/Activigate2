063eb9bd0982290fc39ed60ca04fad45cba242bb
adria <adria@IPAL-MAC.local>
2018-03-29 13:10:31 +0800

    Intro Activity

---
+++ app/src/main/java/fr/cnrs/ipal/activigate2/View/IntroActivity.java
@@ -0,0 +1,33 @@
+package fr.cnrs.ipal.activigate2.View;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.cnrs.ipal.activigate2.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        intent = new Intent(this, MainActivity.class);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(intent);
+            }
+        }, 5000);   //5 seconds
+
+    }
+
+    private void goMainActivity() {
+
+    }
+}
